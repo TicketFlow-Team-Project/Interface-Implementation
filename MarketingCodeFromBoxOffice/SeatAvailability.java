8f827f4ca479d46a6cc32967ac8f445b02b3a8ca
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2025-03-06 09:18:34 +0000

    Added Seat Availability class

---
+++ MarketingCodeFromBoxOffice/SeatAvailability.java
@@ -0,0 +1,9 @@
+class SeatAvailability {
+    String seatNumber;
+    boolean isAvailable;
+
+    public SeatAvailability(String seatNumber, boolean isAvailable) {
+        this.seatNumber = seatNumber;
+        this.isAvailable = isAvailable;
+    }
+}
