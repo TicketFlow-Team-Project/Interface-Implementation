943d17a00d967ba562dce3adfd9292bf2f2288d6
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2025-03-06 09:20:17 +0000

    Made some changes to these implementation of the files

--- MarketingCodeFromBoxOffice/RefundConfirmation.java
+++ MarketingCodeFromBoxOffice/RefundConfirmation.java
@@ -1,10 +1,12 @@
 public class RefundConfirmation {
-    String transactionID;
-    int refundAmount;
-    boolean success;
+  String transactionID;
+  double refundAmount;
+  boolean success;
 
-    public RefundConfirmation(String transactionID, int refundAmount){
+  public RefundConfirmation(String transactionID, double refundAmount, boolean success) {
     this.transactionID = transactionID;
     this.refundAmount = refundAmount;
+    this.success = success;
   }
+
 }
